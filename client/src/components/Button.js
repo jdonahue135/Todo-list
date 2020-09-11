a7269a0aa924c165ff4901dbd9b4d433567e4dfa
Jake Donahue <jdonahue135@gmail.com>
2020-09-11 13:51:10 -0600

    add fontawesome icon to component

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const Button = (props) => {
     return (
-        <div className="btn-container">
+        <div className="btn-container" onClick={props.onClick}> 
+            <FontAwesomeIcon icon={faPlus} className="fa-plus"/>
             <div className="btn-text">{props.text}</div>
         </div>
     )
