217c103b0827a7d5093da1e757453dd5cd977bd8
Jake Donahue <jdonahue135@gmail.com>
2020-09-11 13:52:24 -0600

    dynamically render list using lifecycle hooks and a render function

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,22 +2,35 @@ import React from "react";
 
 import Button from "./Button";
 
-const Sidebar = (props) => {
-    return (
-        <div className="sidebar">
-            <Button text="New project" />
-            <div className="sidebar-content-container">
-                <div className="headline">Projects</div>
-                <div className="project-list">
-                    <div className="project">All Tasks</div>
-                    <div className="project">Today</div>
-                    <div className="project">This Week</div>
-                    <div className="project">Priority</div>
+class Sidebar extends React.Component {
+    componentDidUpdate(prevProps) {
+        if (this.props.selected !== prevProps.selected) {
+            const todoList = document.getElementById("sidebar-" + this.props.selected);
+            todoList.classList = todoList.classList + " project-selected"
+        };
+    };
+    renderList(str, index) {
+        const classList = this.props.selected === index ? "project project-selected" : "project"
+        return (
+            <div key={"sidebar-" + index} onClick={this.props.onListChange} id={"sidebar-" + index} className={classList}>{str}</div>
+        )
+    }
+
+    render() {
+        const todoLists = ["All Tasks", "Priority"];
+        return (
+            <div className="sidebar">
+                <Button text="New Todo" onClick={this.props.onNewTodo}/>
+                <div className="sidebar-content-container">
+                    <div className="headline">Projects</div>
+                    <div className="project-list">
+                        {todoLists.map(list => this.renderList(list, todoLists.indexOf(list)))}
+                    </div>
                 </div>
+                <div className="account"><span className="link">Login</span> or <span className="link">Sign up</span></div>
             </div>
-            <div className="account"><span className="link">Login</span> or <span className="link">Sign up</span></div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Sidebar;
