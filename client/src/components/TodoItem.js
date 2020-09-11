795f54fcef1d943003c63be1934ddfd0e7bf4bd1
Jake Donahue <jdonahue135@gmail.com>
2020-09-11 13:54:12 -0600

    make display dynamic and add CSS animation classes

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -1,14 +1,54 @@
-import React from "react"
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
-const TodoItem = (props) => {
-    return (
-        <div className="todo-item-container">
-            <div className="todo-item">
-                <div className="checkbox" />
-                <div className="todo-item-title">TodoItem</div>
+class TodoItem extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isDone: null,
+            showAnimation: false
+        }
+    };
+    componentDidMount() {
+        this.setState({
+            isDone: this.props.todo.isDone,
+        });
+    };
+    handleClick() {
+        if (this.props.onClick) {
+            this.props.onClick(this.props.todo);
+        };
+    };
+    handleCheckboxClick(e) {
+        //make sure parent onClick event does not fire
+        e.stopPropagation();
+        this.setState({
+            isDone: !this.state.isDone,
+            showAnimation: true,
+        });
+        this.props.onStatusChange(this.props.todo);
+    };
+    render() {
+        let titleClassList = "todo-item-title";
+        let checkboxClass = this.state.isDone ? "checkbox check" : "checkbox"
+        if (this.state.showAnimation) {
+            titleClassList = this.state.isDone ? titleClassList + " strike" : titleClassList + " unstrike"
+        } else if (this.props.todo.isDone) {
+            titleClassList = titleClassList + " done"
+        }
+        return (
+            <div className="todo-item-container" onClick={this.handleClick.bind(this)}>
+                <div className="todo-item">
+                    <div className={checkboxClass} onClick={this.handleCheckboxClick.bind(this)}>
+                        {this.state.isDone ? <FontAwesomeIcon icon={faCheckCircle} className="fa-check" /> : null}
+                    </div>
+                    <div className={titleClassList}>{this.props.todo.title}</div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    };
 }
 
 export default TodoItem;
