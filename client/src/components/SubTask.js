28d08376ee37f7f68b5f4a542eea221441e7b00c
Jake Donahue <jdonahue135@gmail.com>
2020-09-11 13:52:49 -0600

    add component to add subtasks

---
+++ client/src/components/SubTask.js
@@ -0,0 +1,45 @@
+import React from "react";
+import TextareaAutosize from 'react-textarea-autosize';
+
+class SubTask extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            titleInput: ""
+        };
+    };
+    handleInputChange(e) {
+        if (this.state.titleInput.length === 0 && e.target.value.trim().length === 0) {
+            return;
+        } else {
+            this.setState({titleInput: e.target.value});
+        }
+    };
+    handleBlur(e) {
+        if (this.state.titleInput.trim().length > 0) {
+            this.props.onSubTaskSubmit(this.state.titleInput);
+            this.setState({titleInput: ""});
+        };
+    };
+    handleKeyDown(e) {
+        if (e.keyCode === 13 && this.state.titleInput.trim().length > 0) {
+            //enter button pressed
+            this.props.onSubTaskSubmit(this.state.titleInput);
+            this.setState({titleInput: ""});
+        };
+    };
+
+    render() {
+        return (
+            <div className="todo-item-container">
+                <div className="todo-item">
+                    <div className="checkbox" />
+                    <TextareaAutosize className="text-area-autosize sub-task-form" placeholder="Add a new subtask" onChange={this.handleInputChange.bind(this)} value={this.state.titleInput} onBlur={this.handleBlur.bind(this)} onKeyDown={this.handleKeyDown.bind(this)}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SubTask;
