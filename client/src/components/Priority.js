0417fcad54102401f5eec94b4eecf966361cd8d4
Jake Donahue <jdonahue135@gmail.com>
2020-09-18 11:38:41 -0600

    add id to priority component

--- client/src/components/Priority.js
+++ client/src/components/Priority.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Priority = (props) => {
-    return <div className={"priority-btn todo-form-priority" + props.class} onClick={props.onClick}>PRIORITY</div>
+    return <div className={"priority-btn todo-form-priority" + props.class} id={props.id} onClick={props.onClick}>PRIORITY</div>
 };
 
 export default Priority;
