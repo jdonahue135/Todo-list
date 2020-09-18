acee5a5a0d066a3efbd55f4975176690b5fc63cf
Jake Donahue <jdonahue135@gmail.com>
2020-09-18 11:41:20 -0600

    simplify User model by removing name and projects keys

--- models/User.js
+++ models/User.js
@@ -3,11 +3,10 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  name: { type: String, required: true, max: 50 },
+  username: { type: String, required: true, max: 50 },
   password: { type: String, required: true, max: 15 },
-  username: { type: String, required: true, max: 15 },
   joinDate: { type: Date, default: Date.now },
-  projects: [{ type: Schema.Types.ObjectId, ref: "Project" }],
+  todos: [{ type: Schema.Types.ObjectId, ref: "Todo" }],
 });
 
 //Export model
