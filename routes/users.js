3a15513f3f8fc392ddad996d97793fb70b1b6dc5
Jake Donahue <jdonahue135@gmail.com>
2020-09-18 11:32:25 -0600

    add routes for login and signup on post

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var user_controller = require("../controllers/userController");
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* handle log in on POST */
+router.post("/login", user_controller.login);
+
+/* handle sign up on POST */
+router.post("/signup", user_controller.signup);
 
 module.exports = router;
