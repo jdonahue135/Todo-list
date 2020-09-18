8bb2490fd737eb245828b3858a0edba825794259
Jake Donahue <jdonahue135@gmail.com>
2020-09-18 11:37:42 -0600

    implement localStorage for authentication, implement fetch calls to get user todos, implement todo post fetch call to backend, combine update functions into single dynamic function, implement login/logout/signup functions, and make UI more dynamic in render function

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/App.css';
+import { storageAvailable } from "../localStorage";
 
+import LogIn from "./LogIn";
 import TodoForm from "./TodoForm";
 import Sidebar from "./Sidebar";
 import TodoDetail from "./TodoDetail";
@@ -13,30 +15,87 @@ class App extends React.Component {
 
     this.state = {
       selectedList: null,
-      todos: [{title: "dummy todo 0", priority: false, notes: "this is a note", isDone: false, date: Date.now, subTasks: [{title: "subtask 0", isDone: false}, {title: "subtask 1", isDone: true}]},
-      {title: "dummy todo 1", priority: true, notes: "this is another note", isDone: true, date: Date.now, subTasks: [{title: "subtask 2", isDone: true}, {title: "subtask 3", isDone: false}]}],
+      todos: [],
       showOverlay: false,
+      showLogInForm: false,
+      showSignUpForm: false,
       selectedTodo: null,
       user: null,
-    }
-  }
+      jwt: null,
+      usernameInput: "",
+      passwordInput: "",
+      updateBackend: false,
+    };
+  };
   componentDidMount() {
-    this.setState({selectedList: 0})
-  }
+    this.setState({selectedList: 0});
+    if (storageAvailable("localStorage")) {
+      if (
+        localStorage.getItem("jwt") !== "null" &&
+        localStorage.getItem("jwt") !== null &&
+        localStorage.getItem("user") !== null &&
+        localStorage.getItem("user") !== "null"
+      ) {
+        //update state with token and user
+        const jwt = localStorage.getItem("jwt");
+        const user = JSON.parse(localStorage.getItem("user"));
+
+        this.setState({
+          jwt,
+          user
+        });
+      }
+    }
+  };
   componentDidUpdate(prevProps, prevState) {
-    if (this.state.selectedList !== prevState.selectedList) {
+    if (this.state.user && this.state.user !== prevState.user) {
+      this.fetchTodos();
+    }
+    if (this.state.todos && this.state.selectedList !== prevState.selectedList) {
       if (this.state.selectedList === 0) {
         this.setState({selectedTodo: this.state.todos[0]});
       } else {
         this.setState({selectedTodo: this.state.todos.filter(todo => todo.priority === true)[0]});
       }
     }
+    if (prevState.jwt !== this.state.jwt) {
+      //save jwt and user to local storage
+      localStorage.removeItem("jwt");
+      localStorage.removeItem("user");
+      localStorage.setItem("jwt", this.state.jwt);
+      localStorage.setItem("user", JSON.stringify(this.state.user));
+    }
+
+    //update selectedTodo on backend if changed
+
+  };
+  fetchTodos() {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: "Bearer " + this.state.jwt,
+      },
+    };
+
+    const url = "/todos/" + this.state.user._id
+
+    fetch(url, requestOptions)
+      .then(res => res.json())
+      .then(res => {
+        if (!res.success) {
+          console.log(res.message);
+          return;
+        } else {
+          this.setState({todos: res.todos})
+        }
+      })
   }
   toggleOverlay() {
     this.setState({showOverlay: !this.state.showOverlay})
-  }
+  };
   handleFormSubmit(data) {
-    this.setState({showOverlay: false})
+    this.setState({showOverlay: false});
     const todo = {
       title: data.titleInput,
       priority: data.priority,
@@ -47,29 +106,40 @@ class App extends React.Component {
     };
     let todos = [...this.state.todos];
     todos.push(todo);
-    this.setState({todos});
-  }
+    if (!this.state.user) {
+      this.setState({todos});
+    } else {
+
+      //TODO: Fetch request if user is logged in
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: "Bearer " + this.state.jwt,
+        },
+        body: JSON.stringify({
+          user: this.state.user,
+          todo: todo,
+        }),
+      };
+
+      fetch("/todos", requestOptions)
+        .then(res => res.json())
+        .then(res => {
+          if (!res.success) {
+            console.log(res.message)
+          } else {
+            this.setState({todos: res.todos})
+          }
+        })
+    }
+  };
   handleListChange(e) {
     const selectedList = parseInt(e.target.id.replace("sidebar-", ""));
     this.setState({selectedList});
-  }
+  };
   handleSelectedTodo(selectedTodo) {
     this.setState({selectedTodo});
-  }
-  handleNotesChange(notesText, todo) {
-    let todos = [...this.state.todos];
-    todos[todos.indexOf(todo)].notes = notesText;
-    this.setState({todos});
-  };
-  handleTitleChange(titleText, todo) {
-    let todos = [...this.state.todos];
-    todos[todos.indexOf(todo)].title = titleText;
-    this.setState({todos});
-  };
-  handlePriorityChange() {
-    let todos = [...this.state.todos];
-    todos[todos.indexOf(this.state.selectedTodo)].priority = !todos[todos.indexOf(this.state.selectedTodo)].priority;
-    this.setState({todos});
   };
   handleTodoStatusToggle(todo) {
     let todos = [...this.state.todos];
@@ -77,6 +147,21 @@ class App extends React.Component {
     const target = todo.subTasks ? todos[todos.indexOf(todo)]: todos[todos.indexOf(this.state.selectedTodo)].subTasks[todos[todos.indexOf(this.state.selectedTodo)].subTasks.indexOf(todo)];
     target.isDone = !target.isDone;
     this.setState({todos});
+  };
+  handleTodoUpdate(e) {
+    let todos = [...this.state.todos];
+    let todoIndex = todos.indexOf(this.state.selectedTodo)
+    if (!e.target.value) {
+      todos[todoIndex][e.target.id] = !todos[todoIndex][e.target.id];
+    } else {
+      todos[todoIndex][e.target.id] = e.target.value;
+    }
+    const updateBackend = this.state.user ? true : false;
+    this.setState({
+      todos: todos,
+      selectedTodo: todos[todoIndex],
+      updateBackend: updateBackend,
+    });
   };
   handleSubTaskSubmit(title) {
     let todos = [...this.state.todos];
@@ -86,8 +171,95 @@ class App extends React.Component {
     };
     todos[todos.indexOf(this.state.selectedTodo)].subTasks.push(subTask);
     this.setState({todos});
+  };
+  handleAccountClick(e) {
+    if (e.target.id === "log-in") {
+      this.setState({showLogInForm: true});
+    } else {
+      this.setState({showSignUpForm: true});
+    }
+  };
+  handleCancelClick() {
+    this.setState({
+      usernameInput: "", 
+      passwordInput: "",
+      showSignUpForm: false,
+      showLogInForm: false
+    });
+  };
+  handleInputChange(e) {
+    this.setState({[e.target.name + "Input"] : e.target.value})
+  };
+  handleLogIn() {
+    if (!this.state.usernameInput || !this.state.passwordInput) return;
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: this.state.usernameInput,
+        password: this.state.passwordInput,
+      }),
+    };
+
+    fetch("/users/login", requestOptions)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.success === false) {
+          //this.setState({ showLoginWarning: true });
+        } else {
+          this.setState({
+            jwt: res.token,
+            user: res.user,
+            showLogInForm: false,
+            //showLoginWarning: false,
+          });
+        }
+      })
+      .catch((err) => console.log(err));  
+  };
+  handleSignUp() {
+    if (!this.state.usernameInput || !this.state.passwordInput) return;
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: this.state.usernameInput,
+          password: this.state.passwordInput,
+          todos: this.state.todos,
+        }),
+      };
+    fetch("/users/signup", requestOptions)
+      .then((res) => res.json())
+      .then((res) =>
+        this.setState({
+          jwt: res.token,
+          user: res.user,
+          showSignUpForm: false,
+        })
+      )
+      .catch((err) => console.log(err));
+  };
+  handleLogOut() {
+    //remove user and jwt from localStorage
+    localStorage.clear();
+
+    //logs user out of state and clears tweet cache
+    this.setState({
+      user: null,
+      jwt: null,
+      todos: [],
+    });
   }
   render() {
+    let isButtonEnabled = false;
+    if (this.state.usernameInput.length > 0 && this.state.passwordInput.length > 0) {
+      isButtonEnabled = true;
+    }
+    const isLoggedIn = this.state.user ? true : false;
     return (
       <div className="App">
         {this.state.showOverlay 
@@ -96,23 +268,35 @@ class App extends React.Component {
               <div className="overlay" />
             </div> 
           : null}
-        <Sidebar onListChange={this.handleListChange.bind(this)} selected={this.state.selectedList} onNewTodo={this.toggleOverlay.bind(this)}/>
+        {this.state.showLogInForm 
+          ? <div>
+              <LogIn onInputChange={this.handleInputChange.bind(this)} handleCancel={this.handleCancelClick.bind(this)} signUp={false} onSubmit={this.handleLogIn.bind(this)} buttonEnabled={isButtonEnabled}/>
+              <div className="overlay" />
+            </div> 
+          : null}
+          {this.state.showSignUpForm 
+          ? <div>
+              <LogIn onInputChange={this.handleInputChange.bind(this)} handleCancel={this.handleCancelClick.bind(this)} signUp={true} onSubmit={this.handleSignUp.bind(this)} buttonEnabled={isButtonEnabled}/>
+              <div className="overlay" />
+            </div> 
+          : null}
+        <Sidebar user={isLoggedIn} handleLogOut={this.handleLogOut.bind(this)} onListChange={this.handleListChange.bind(this)} selected={this.state.selectedList} onNewTodo={this.toggleOverlay.bind(this)}/>
         <div className="background-container">
           <img className="background" src={background} alt="background" />
         </div>
         <div className="main">
           <div className="title-container">
-            <div className="title">{this.state.user ? "Welcome, " + this.state.user : <div><span className="link">Login</span> or <span className="link">Sign up</span> to save todos</div>}</div>
+            <div className="title">{this.state.user ? "Welcome, " + this.state.user.username : <div><span className="link" id="log-in" onClick={this.handleAccountClick.bind(this)}>Log in</span> or <span className="link" id="sign-up" onClick={this.handleAccountClick.bind(this)}>Sign up</span> to save todos</div>}</div>
           </div>
           <div className="content">
             <TodoList selectedList={this.state.selectedList} onTodoClick={this.handleSelectedTodo.bind(this)} todos={this.state.todos} onStatusChange={this.handleTodoStatusToggle.bind(this)} />
-            {this.state.selectedTodo ? <TodoDetail todo={this.state.selectedTodo} onTitleChange={this.handleTitleChange.bind(this)} onNotesChange={this.handleNotesChange.bind(this)} onPriorityChange={this.handlePriorityChange.bind(this)} onStatusChange={this.handleTodoStatusToggle.bind(this)} onSubTaskSubmit={this.handleSubTaskSubmit.bind(this)} /> : null}
+            {this.state.selectedTodo ? <TodoDetail todo={this.state.selectedTodo} onChange={this.handleTodoUpdate.bind(this)} onStatusChange={this.handleTodoStatusToggle.bind(this)} onSubTaskSubmit={this.handleSubTaskSubmit.bind(this)} /> : null}
           </div>
         </div>
         
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
