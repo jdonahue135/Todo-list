cf7de3484e848f53f50d0d6a4a19c9c80aeb10f5
Jake Donahue <jdonahue135@gmail.com>
2020-09-19 11:20:21 -0600

    add controller for subTask routes and implement create and update functions

---
+++ controllers/subTaskController.js
@@ -0,0 +1,60 @@
+var Todo = require("../models/Todo");
+var SubTask = require("../models/SubTask");
+
+const { body } = require("express-validator");
+const { sanitizeBody } = require("express-validator");
+
+exports.sub_task_post = (req, res, next) => {
+
+    // Validate fields.
+    body('title', 'Title must not be empty.').trim().isLength({ min: 1 }),
+
+    // Sanitize fields.
+    sanitizeBody('title').escape(),
+
+    Todo.findById(req.body.todo._id)
+        .select('subTasks')
+        .exec((err, theTodo) => {
+            if (err) res.json({ success: false, message: "Error, API failed to fetch info from db" });
+            else {
+                if (!theTodo) res.json({ success: false, message: "Todo not found" });
+                else {
+                    const newSubTask = new SubTask({
+                        title: req.body.subTask.title,
+                        isDone: req.body.subTask.isDone,
+                    });
+                    newSubTask.save(err => {
+                        if (err) {res.json({success: false, message: "ERROR: unable to save sub task"})}
+                    })
+                    theTodo.subTasks.push(newSubTask);
+                    theTodo.save(err => {
+                        if (err) {res.json({success: false, message: "ERROR: unable to save sub task"})}
+                        else res.json({success: true, message: "sub task saved"});
+                    });
+                }
+            }
+        })
+};
+
+exports.sub_task_update = (req, res, next) => {
+    // Validate fields.
+    body('title', 'Title must not be empty.').trim().isLength({ min: 1 }),
+
+    // Sanitize fields.
+    sanitizeBody('title').escape();
+    
+    const originalSubTask = SubTask.findById(req.params.subtaskid)
+    const subTask = new SubTask({
+        _id: req.params.subtaskid,
+        title: req.body.subTask.title,
+        isDone: req.body.subTask.isDone,
+    });
+    if (originalSubTask === subTask) {
+        return;
+    } else {
+        SubTask.findByIdAndUpdate(req.params.subtaskid, subTask, err => {
+            if (err) res.json({success: false, err: err});
+            else res.json({success: true, message: "sub task updated"});
+        });
+    };
+}
