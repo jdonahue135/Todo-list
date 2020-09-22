f0af2d1060a5e93d982db53cfeda4aa56c035098
Jake Donahue <jdonahue135@gmail.com>
2020-09-22 11:06:59 -0600

    make SubTask form dynamic by adding a ref and selecting when component mounts

--- client/src/components/SubTask.js
+++ client/src/components/SubTask.js
@@ -8,6 +8,14 @@ class SubTask extends React.Component {
         this.state = {
             titleInput: ""
         };
+        this.myRef = React.createRef();
+    };
+    componentDidMount() {
+        if (this.props.subTask) {
+            this.setState({titleInput: this.props.subTask.title});
+            const node = this.myRef.current;
+            node.select()
+        };
     };
     handleInputChange(e) {
         if (this.state.titleInput.length === 0 && e.target.value.trim().length === 0) {
@@ -18,14 +26,14 @@ class SubTask extends React.Component {
     };
     handleBlur(e) {
         if (this.state.titleInput.trim().length > 0) {
-            this.props.onSubTaskSubmit(this.state.titleInput);
+            this.props.onSubmit ? this.props.onSubmit(this.state.titleInput) : this.props.onUpdate(this.state.titleInput);
             this.setState({titleInput: ""});
         };
     };
     handleKeyDown(e) {
         if (e.keyCode === 13 && this.state.titleInput.trim().length > 0) {
             //enter button pressed
-            this.props.onSubTaskSubmit(this.state.titleInput);
+            this.props.onSubmit ? this.props.onSubmit(this.state.titleInput) : this.props.onUpdate(this.state.titleInput);
             this.setState({titleInput: ""});
         };
     };
@@ -36,6 +44,7 @@ class SubTask extends React.Component {
                 <div className="todo-item">
                     <div className="checkbox" />
                     <TextareaAutosize 
+                        ref={this.myRef}
                         className="text-area-autosize sub-task-form" 
                         placeholder="Add a new subtask" 
                         onChange={this.handleInputChange.bind(this)} 
